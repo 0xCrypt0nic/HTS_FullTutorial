8e4696090e260a37cd359ccfbaa66f723c07f338
0xCrypt0nic <n.greneche45@gmail.com>
2026-01-16 15:53:11 +0100

    Int type

--- Module1/InfosPersos/Program.cs
+++ Module1/InfosPersos/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("Bonjour, saisir votre prénom :");
 string? prenom = Console.ReadLine();
+Console.WriteLine("Merci, saisir votre âge :");
+int age = 0;
+int.TryParse(Console.ReadLine(), out age);
 Console.Clear();
-Console.WriteLine($"Bonjour {prenom}");
+Console.WriteLine($"Bonjour {prenom}, vous avez {age} ans.");
--- MonPremierProjet/obj/Debug/net10.0/MonPremierProjet.AssemblyInfo.cs
+++ MonPremierProjet/obj/Debug/net10.0/MonPremierProjet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonPremierProjet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7992f1f53c3cc7b3d85c92b465e32d1db77130f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fbc604dd053ad0a2e481f73765ac7e19e9bc6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonPremierProjet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonPremierProjet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
