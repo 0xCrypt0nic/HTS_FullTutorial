8893ff881bb8bc28c60e2418abf886efb44c9d02
0xCrypt0nic <n.greneche45@gmail.com>
2026-01-18 15:36:14 +0100

    For loop

---
+++ Module1/TestingEx/Program.cs
@@ -0,0 +1,7 @@
+﻿// Tests For Loop
+
+int[] tabOfInteger = [7, 9, 6, 12, 11, 45];
+for (int i = 0; i < tabOfInteger.Length; i++)
+{
+    Console.WriteLine($"Data of position {i + 1} in tabOfInteger : {tabOfInteger[i]}");
+}
